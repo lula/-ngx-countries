1691962d727239ccadaa70907b590fdfb07191f5
llulani <luca.lulani@trilog.it>
2020-03-24 10:08:08 +0100

    material, examples

---
+++ projects/ngx-countries/material/src/lib/material.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-material',
+  template: `
+    <p>
+      material works!
+    </p>
+  `,
+  styles: []
+})
+export class MaterialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
